30bfda2dbd459785a3c55e6bec2bbdc9bc2e2014
Ashton K Ellis <ashtonkellis@gmail.com>
2018-05-29 16:20:56 -0700

    meals rendering to search page

--- scripts/views/search-view.js
+++ scripts/views/search-view.js
@@ -4,10 +4,12 @@ var app = app || {};
 (function (module) {
   const searchView = {};
 
-  searchView.all = [];
-
   searchView.initSearchPage = function () {
-    
+    app.Meal.fetchAll(app.searchView.renderAll);
+  }
+
+  searchView.renderAll = () => {
+    app.Meal.all.forEach(meal => $('#meals').append(app.render('meal-template', meal)))
   }
 
 
