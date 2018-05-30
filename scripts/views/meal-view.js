77c2c84fc792d1d5e2b07a54cf693cce0bbd1c2e
Ashton K Ellis <ashtonkellis@gmail.com>
2018-05-29 23:04:03 -0700

    meal-view iffe set up

---
+++ scripts/views/meal-view.js
@@ -0,0 +1,25 @@
+'use strict';
+var app = app || {};
+
+(function (module) {
+  const mealView = {};
+
+  mealView.initMealPage = function () {
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  module.mealView = mealView;
+
+})(app);
