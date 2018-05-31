b352ab8a34acaec4d07c8fce3e42844219d33579
Kevin Hwangpo <hwangpo3@gmail.com>
2018-05-31 09:31:35 -0700

    Making an attempt to add strik through text on click of check box

--- scripts/views/meal-view.js
+++ scripts/views/meal-view.js
@@ -1,5 +1,6 @@
 'use strict';
 var app = app || {};
+var counter = 1, counter2 = 1;
 
 (function (module) {
 
@@ -11,23 +12,30 @@ var app = app || {};
     $('#meal-ingredients').empty();
     $('#meal-instructions').empty();
 
-    $('#meal-ingredients').on('change',(e) => {
-      $(this).parent().css('text-decoration', 'line-through');
-    });
+    // $('#meal-ingredients').on('change',(e) => {
+    //   $(this).parent().css('text-decoration', 'line-through');
+    // });
 
+    
     $.get(`${app.ENVIRONMENT.apiUrl}/api/json/recipe/${mealId}`)
       .then(mealData => {
         $('#meal-name').text(mealData.name);
         $('#meal-image').attr('src', mealData.thumb);
 
+        // console.log(mealData.ingredient);
+
         mealData.ingredients.forEach(ingredient => {
-          $('#meal-ingredients').append(`<li><input id="strikeThrough" type="checkbox">${ingredient.measure} ${ingredient.name}</li>`)
+          $('#meal-ingredients').append(`<li id="cstrikeThrough${counter}"><input id="strikeThrough${counter}" type="checkbox">${ingredient.measure} ${ingredient.name}</li>`);
+          counter++;
         })
 
         mealData.instructions.forEach(instruction => {
-          $('#meal-instructions').append(`<li class="ingredient"><input id="strikeThrough" type="checkbox">${instruction.body}</li>`);
+          $('#meal-instructions').append(`<li id="rstrikeThrough${counter} class="ingredient"><input id="strikeThrough${counter2}" type="checkbox">${instruction.body}</li>`);
+          counter2++;
         })
-      })
+      });
+
+   
 
 
     // console.log($('#meal-image').attr('id'));
@@ -39,6 +47,32 @@ var app = app || {};
 
     //   }
     // });
-  }
-  module.mealView = mealView;
-})(app)
+//   }
+//   module.mealView = mealView;
+ 
+//   $(document).ready(function() {
+//     $("input[type='checkbox']").keypress(function (){
+//       console.log("printed:" + counter);
+//     });
+//   });
+
+// })(app);
+
+// $(function () {
+//   $("#strikeThrough1").change(function(){
+//     alert("clicked");
+// });
+// })
+
+// console.log("printed:" + counter);
+// for (var i = 0; i < counter; i++) {
+//   var appendID = '#strikeThrough' + counter;
+//   //var append2 = 'c'+appendID;
+//   $(appendID).change(function () {
+//     console.log("clicked");
+//   });
+//}
+
+
+
+
