02b1cc9f15c4383c5fe168f3f6d3b9a6cf6b1567
Kevin Hwangpo <hwangpo3@gmail.com>
2018-05-30 17:44:59 -0700

    Attempting to add strike through

--- scripts/views/meal-view.js
+++ scripts/views/meal-view.js
@@ -11,19 +11,34 @@ var app = app || {};
     $('#meal-ingredients').empty();
     $('#meal-instructions').empty();
 
+    $('#meal-ingredients').on('change',(e) => {
+      $(this).parent().css('text-decoration', 'line-through');
+    });
+
     $.get(`${app.ENVIRONMENT.apiUrl}/api/json/recipe/${mealId}`)
       .then(mealData => {
         $('#meal-name').text(mealData.name);
         $('#meal-image').attr('src', mealData.thumb);
 
         mealData.ingredients.forEach(ingredient => {
-          $('#meal-ingredients').append(`<li><input type="checkbox">${ingredient.measure} ${ingredient.name}</li>`)
+          $('#meal-ingredients').append(`<li><input id="strikeThrough" type="checkbox">${ingredient.measure} ${ingredient.name}</li>`)
         })
 
         mealData.instructions.forEach(instruction => {
-          $('#meal-instructions').append(`<li><input type="checkbox">${instruction.body}</li>`);
+          $('#meal-instructions').append(`<li class="ingredient"><input id="strikeThrough" type="checkbox">${instruction.body}</li>`);
         })
       })
+
+
+    // console.log($('#meal-image').attr('id'));
+
+    // $('#strikeThrough').change(function () {
+    //   // console.log('changed')
+    //   if (this.checked) {
+    //     console.log($(this).wrap());
+
+    //   }
+    // });
   }
   module.mealView = mealView;
 })(app)
